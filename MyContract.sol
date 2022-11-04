b516ae9dd561c2ef6c2292b656a274f6f311254b
Sergio Martin Rubio <me@sergiomartinrubio.com>
2022-11-04 17:57:55 +0100

    Add smart contract and deployment script

---
+++ MyContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract MyContract {
+    function helloWorld() public view returns (string memory) {
+        return "Hello World!";
+    }
+}
