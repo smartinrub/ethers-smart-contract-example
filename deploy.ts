f90f95c9e542b27ade1c9ec703ee579deedef96d
Sergio Martin Rubio <me@sergiomartinrubio.com>
2022-11-07 22:53:38 +0100

    add ts files

---
+++ deploy.ts
@@ -0,0 +1,30 @@
+import {ethers} from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
+  let wallet = ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD!
+  )
+  wallet = await wallet.connect(provider)
+  const abi = fs.readFileSync("./MyContract_sol_MyContract.abi", "utf8")
+  const binary = fs.readFileSync("./MyContract_sol_MyContract.bin", "utf8")
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Start deploying")
+  const contract = await contractFactory.deploy()
+  await contract.deployTransaction.wait(1)
+
+  const helloWorld = await contract.helloWorld()
+  console.log(helloWorld)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
